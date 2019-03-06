a8b9f28143338e6ca804218adba9464ca0d73922
Phi Hai Nguyen <phihai93@gmail.com>
2019-03-06 01:19:23 -0800

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+//4 buttons which holds different values which needs click events value of 1-12
+//computer needs to generate random number for #score must be number of 19-120
+//function on when crystals are clicked on its value will be added
+
+
